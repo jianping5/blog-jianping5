4257c5b53cd8e6de09a0ec029e7556f97bb29961
jianping5 <2712748478@qq.com>
2023-04-15 17:42:16 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:jianping5/jianping5.github.io.git main
+git push -f git@github.com:jianping5/jianping5.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:jianping5/blog.git main:gh-pages
+# git push -f git@github.com:jianping5/blog.git master:gh-pages
 
 cd -
